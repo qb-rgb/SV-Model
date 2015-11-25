1ac021b5934bf21ab4d75a2d6828cd3ece490624
qw0te <baert.quentin@gmail.com>
2015-11-25 16:11:40 +0100

    Ajoute des types pour représenter une molécule et une solution

--- src/main/scala/model/model.scala
+++ src/main/scala/model/model.scala
@@ -1,2 +1,12 @@
-/** Contain the classes to model a chimical reaction system. */
-package object model
+/** Contain the classes to model a chemical reaction system. */
+package object model {
+
+  /** A molecule is represented by its name. */
+  type Molecule = String
+
+  /** A solution is a map which associate a molecule with its quantity in the
+    * solution.
+    */
+  type Solution = Map[Molecule, Int]
+
+}
