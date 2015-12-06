59f5c54e16925f41815ebeaaeafdc6f55f6f7800
qw0te <baert.quentin@gmail.com>
2015-12-06 17:03:09 +0100

    Modifie le système de test

--- src/main/scala/sim/Test.scala
+++ src/main/scala/sim/Test.scala
@@ -4,17 +4,13 @@ import sim._
 object Test {
 
   def main(args: Array[String]): Unit = {
-    val initSol = Map("A" -> 100000)
+    val initSol = Map("S" -> 100000, "E" -> 0, "P" -> 0)
 
-    val react1 = Map("A" -> 2)
-    val prod1: Solution = Map()
-    val r1 = Reaction(react1, prod1, 0.002)
+    val react1 = Map("S" -> 1, "E" -> 1)
+    val prod1 = Map("P" -> 1, "E" -> 1)
+    val r1 = Reaction(react1, prod1, 0.1)
 
-    // val react2 = Map("C" -> 1, "A" -> 2)
-    // val prod2 = Map("B" -> 3)
-    // val r2 = Reaction(react2, prod2, 1)
-
-    val simulator = new Simulator(initSol, Set(r1), 0.01)
+    val simulator = new Simulator(initSol, Set(r1), 10)
 
     simulator.simule
   }
